02c48a94db961f0d101302aa7dc39ec364796ed1
Thilo Plasger <ekzodus4.17@gmail.com>
2025-12-28 00:20:58 +0100

    Add parsing function

--- evaluators/expressions.go
+++ evaluators/expressions.go
@@ -3,23 +3,73 @@ package evaluators
 import "github.com/BinaryGhost/alpaci/lexing"
 
 type Expression struct {
-	operator Operator
-	operands []Atom
-	children []Expression
+	Kind     ExpressionKind
+	Operator Operator
+	Left     *Expression
+	Right    *Expression
+	Atom     Atom
 }
 
-func ParseExpression(tl *lexing.TokenList) {
-	// get 1st token (= first)
-	// is it an operator or atom?
-	//
-	// peek to next token
-	// 	first is operator -> eof -> error
-	// 	first is operator -> operator -> error
-	// 	first is operator -> atom -> no error if (+/-)
-	// consume token
-	// next Token
-	// ---------------
+func (e *Expression) Eval() {}
 
+func ParseExpression(tl *lexing.TokenList, min_bp int) Expression {
+	var lhs Expression
+	first_tok, err := tl.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	switch first_tok.Type {
+	case lexing.Ident:
+		lhs = Expression{Atom: MakeIdentAtom(tl)}
+	case lexing.Number:
+		lhs = Expression{Atom: MakeNumberAtom(tl)}
+	case lexing.Lparenth:
+		tl.Next()
+
+		lhs = ParseExpression(tl, 0)
+		if tok, err := tl.Current(); err != nil || tok.Type != lexing.Rparenth {
+			panic("Expected ')'")
+		}
+
+		tl.Next()
+	default:
+		panic("Bad token")
+	}
+
+	for {
+		var op Operator
+		op_tok, err := tl.Current()
+		if err != nil {
+			panic(err)
+		}
+
+		if !IsInfixOp(op_tok.Type) {
+			break
+		}
+
+		l_bp, r_bp := GetInfixBindingPower(op_tok.Type)
+		if l_bp < min_bp {
+			break
+		}
+
+		op.Column = op_tok.Column
+		op.Type = op_tok.Type
+		op.Val = op_tok.Value
+
+		tl.Next()
+
+		rhs := ParseExpression(tl, r_bp)
+
+		lhs = Expression{
+			Kind:     Infix,
+			Operator: op,
+			Left:     &lhs,
+			Right:    &rhs,
+		}
+	}
+
+	return lhs
 }
 
 func (e *Expression) String() string { return "" }
--- evaluators/operands.go
+++ evaluators/operands.go
@@ -3,10 +3,10 @@ package evaluators
 import "github.com/BinaryGhost/alpaci/lexing"
 
 type Operator struct {
-	Type lexing.TokType
+	Val    any
+	Type   lexing.TokType
+	Column int
 }
-
-type BindingPowers = [2]int
 
 const EMPTY = -99
 const NO_OP = 0
@@ -20,6 +20,31 @@ const (
 	Prefix
 )
 
-func (op *Operator) GetBindingpower(kind ExpressionKind) BindingPowers {
-	return BindingPowers{1, 2}
+func GetPrefixBindingPower(op lexing.TokType) (int, int) {
+	switch op {
+	case lexing.Plus_a, lexing.Minus_a:
+		return EMPTY, 5
+	default:
+		panic("Bad operator")
+	}
+}
+
+func GetInfixBindingPower(op lexing.TokType) (int, int) {
+	switch op {
+	case lexing.Plus_a, lexing.Minus_a:
+		return 1, 2
+	case lexing.Div_a, lexing.Mult_a:
+		return 3, 4
+	default:
+		panic("Bad operator")
+	}
+}
+
+func IsInfixOp(op lexing.TokType) bool {
+	switch op {
+	case lexing.Div_a, lexing.Mult_a, lexing.Mod_a, lexing.Plus_a, lexing.Minus_a:
+		return true
+	default:
+		return false
+	}
 }
